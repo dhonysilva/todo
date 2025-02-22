afdbaca65efb3d9282ffc9b16c0c97b75708e949
Dhony Silva <dhony.web@gmail.com>
2025-02-22 12:17:37 -0300

    Create functions to interect with processes while they still running

--- lib/database_server.ex
+++ lib/database_server.ex
@@ -24,4 +24,16 @@ defmodule DatabaseServer do
     Process.sleep(2000)
     "#{query_def} result"
   end
+
+  def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self(), query_def})
+  end
+
+  def get_result do
+    receive do
+      {:query_result, result} -> result
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
 end
