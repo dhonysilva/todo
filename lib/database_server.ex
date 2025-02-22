8512a4c66bd1c29a75963ed579fb07929f1082fc
Dhony Silva <dhony.web@gmail.com>
2025-02-22 12:43:40 -0300

    Keeping a process state, extending the database with a connection

--- lib/database_server.ex
+++ lib/database_server.ex
@@ -2,27 +2,30 @@ defmodule DatabaseServer do
   # The interface Function
   # Stats the loop concurrently
   def start do
-    spawn(&loop/0)
+    spawn(fn ->
+      connection = :rand.uniform(1000)
+      loop(connection)
+    end)
   end
 
   # Handles one message
-  defp loop do
+  defp loop(connection) do
     # ↓ Awaits a message
     receive do
       {:run_query, caller, query_def} ->
         # ↓ Runs the query and sends the response to the caller
-        query_result = run_query(query_def)
+        query_result = run_query(connection, query_def)
         send(caller, {:query_result, query_result})
     end
 
     # Recursively calls itself
-    loop()
+    loop(connection)
   end
 
-  defp run_query(query_def) do
+  defp run_query(connection, query_def) do
     # ↓ Simulates a long-running query
     Process.sleep(2000)
-    "#{query_def} result"
+    "Connection #{connection}: #{query_def} result"
   end
 
   def run_async(server_pid, query_def) do
