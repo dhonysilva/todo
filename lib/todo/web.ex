ab6b852e41b4bb2748de24fb25fa62cced418839
Dhony Silva <dhony.web@gmail.com>
2025-04-18 11:50:03 -0300

    Add entries get method to the Web module

--- lib/todo/web.ex
+++ lib/todo/web.ex
@@ -12,6 +12,8 @@ defmodule Todo.Web do
     )
   end
 
+  # Adding a list
+  # curl -data "http://localhost:5454/add_entry?list=bob&date=2023-12-19&title=Dentist"
   post "/add_entry" do
     conn = Plug.Conn.fetch_query_params(conn)
     list_name = Map.fetch!(conn.params, "list")
@@ -25,5 +27,27 @@ defmodule Todo.Web do
     conn
     |> Plug.Conn.put_resp_content_type("text/plain")
     |> Plug.Conn.send_resp(200, "OK")
+  end
+
+  # Retrieving entries
+  # curl "http://localhost:5454/entries?list=bob&date=2023-12-19"
+  get "/entries" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    entries =
+      list_name
+      |> Todo.Cache.server_process()
+      |> Todo.Server.entries(date)
+
+    formated_entries =
+      entries
+      |> Enum.map(&"#{&1.date} #{&1.title}")
+      |> Enum.join("\n")
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, formated_entries)
   end
 end
